00f78b3a0b1bbdc07ce3c0d2fd19b0ef5014da61
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-25 20:37:26 +0530

    Callbacks

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log('Before')
+
+getUser(1,(user) =>{
+    console.log('User', user);
+    getRepositories(user.gitHubUsername,(repos)=>{
+        console.log('Repos',repos)
+    });
+});
+
+console.log('After')
+
+
+function getUser(id,callback){
+    setTimeout(() => {
+        console.log('Reading a user from a Database');
+        callback({id:id,gitHubUsername:'harshit'});
+    }, 2000);
+}
+
+
+function getRepositories(username,callback){
+    setTimeout(()=>{
+        console.log('Calling GitHub API...');
+        callback(['repo1','repo2','repo3']);
+    },2000);
+}
